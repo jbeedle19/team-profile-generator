0f939963d6bc43f32f1453c60ba038f159f67423
jbeedle19 <josh.beedle@gmail.com>
2021-01-09 10:17:09 -0500

    tested and created getId method

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,23 @@
-const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
+// Solution has the employee id as a number and not string
+// Will this cause and issue later?
 
 test('creates an employee object', () => {
-    const employee = new Employee('Josh');
+    const employee = new Employee('Josh', '123', 'josh@mail.com');
 
     expect(employee.name).toBe('Josh');
-    expect(employee.id).toEqual(expect.any(String));
-    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.id).toBe('123');
+    expect(employee.email).toBe('josh@mail.com');
 });
 
 test("gets employee's name", () => {
     const employee = new Employee('Josh');
 
     expect(employee.getName()).toBe('Josh');
-})
+});
+
+test("gets employee's id", () => {
+    const employee = new Employee('Josh', '123', 'josh@mail.com');
+
+    expect(employee.getId()).toBe('123');
+});
