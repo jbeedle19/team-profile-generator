ef34db2f0d034eb3c51682e648a8c179ab6153b7
jbeedle19 <josh.beedle@gmail.com>
2021-01-12 11:07:17 -0500

    added writeFile method

--- Index.js
+++ Index.js
@@ -1,1 +1,16 @@
 console.log("Hello, Josh")
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'HTML file created! Take a look at the new file in the dist folder!'
+            });   
+        });
+    });
+};
