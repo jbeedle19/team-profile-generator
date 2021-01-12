6a52f3041eb6adb8c67c045f84425ddaa7485388
jbeedle19 <josh.beedle@gmail.com>
2021-01-12 11:25:48 -0500

    added prompts/validation/defaults for Manager info

--- Index.js
+++ Index.js
@@ -1,4 +1,64 @@
-console.log("Hello, Josh")
+// Include packages/methods/etc. needed for this app
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Prompt user with questions about Manager
+const promptManagerInfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?",
+            default: "Josh",
+            validate: managerNameInput => {
+                if (managerNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's name!");
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the team manager's ID?",
+            default: "019",
+            validate: managerIdInput => {
+                if (managerIdInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's ID!");
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email?",
+            default: "josh.beedle@gmail.com",
+            validate: managerEmailInput => {
+                if (managerEmailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's email address!");
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerOffice',
+            message: "What is the team manager's office number?",
+            default: "A1",
+            validate: managerIdInput => {
+                if (managerIdInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's ID!");
+                }
+            }
+        },
+    ])
+}
 
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
